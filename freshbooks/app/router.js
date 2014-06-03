3f90ad9deb5ff68d2beba4da25262c963e46196c
unknown <neil.anderson@DEV-NEILA.red-gate.com>
2014-06-03 15:06:29 +0100

    added a few hbs files

--- freshbooks/app/router.js
+++ freshbooks/app/router.js
@@ -3,6 +3,18 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('home', function(){
+    this.resource('overview');
+    this.resource('refer');
+    this.resource('pricing');
+    this.resource('stamps');
+  });
+  this.resource('people');
+  this.resource('invoices');
+  this.resource('estimates');
+  this.resource('expenses');
+  this.resource('time');
+  this.resource('reports');
 });
 
 export default Router;
