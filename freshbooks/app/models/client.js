a504ceb968e41c5da68c6232fccd16c952119566
eneiand <neil.t.anderson@gmail.com>
2014-06-07 23:11:37 +0100

    start work on new client form and model

--- freshbooks/app/models/client.js
+++ freshbooks/app/models/client.js
@@ -1,7 +1,15 @@
 export default DS.Model.extend({
   organization: DS.attr('string'),
-  contact: DS.attr('string'),
-  lastLogin: DS.attr('string'),
-  email: DS.attr('string'),
-  phone: DS.attr('string')
+  currency: DS.attr('string', 'GBP'),
+  language: DS.attr('string', 'English'),
+  sendInvoicesByEmail: DS.attr('boolean', true),
+  sendInvoicesByPost: DS.attr('boolean', false),
+  contacts: DS.hasMany('contact'),
+  country: DS.attr('string'),
+  addresses: DS.hasMany('address'),
+  industry: DS.attr('string'),
+  companySize: DS.attr('string'),
+  businessPhone: DS.attr('string'),
+  fax: DS.attr('string'),
+  notes: DS.attr('string')
 });
---
+++ freshbooks/app/models/contact.js
@@ -0,0 +1,7 @@
+export default DS.Model.extend({
+  email: DS.attr('string'),
+  homePhone: DS.attr('string'),
+  firstName: DS.attr('string'),
+  lastName: DS.attr('string'),
+  mobilePhone: DS.attr('string')  
+});
